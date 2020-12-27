d8259661d4c2d243b608d64addf36b210d936dd2
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-27 16:24:04 +0530

    Initiazlize Django Project

--- Music_Controller/Music_Controller/wsgi.py
+++ Music_Controller/Music_Controller/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for music_con project.
+WSGI config for Music_Controller project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_con.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Music_Controller.settings')
 
 application = get_wsgi_application()
