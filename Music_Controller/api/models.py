a653400215fd0f90706366368c0c530f71102d0d
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-28 12:58:39 +0530

    Generate unique code and database models

--- Music_Controller/api/models.py
+++ Music_Controller/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import string
+import random
 # Create your models here.
 
 
@@ -8,3 +9,15 @@ class Room(models.Model):
     defcode = ''
     code = models.CharField(max_length=max_len, default=defcode, unique=True)
     host = models.CharField(max_length=20, unique=True)
+    guest_can_pause = models.BooleanField(null=False,default=False)
+    votes_to_skip = models.IntegerField(null=False,default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def gen_unique_code():
+    length = Room.max_len
+    while True:
+        code = ''.join(random.choice(string.ascii_uppercase,k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break 
+    return code
+
